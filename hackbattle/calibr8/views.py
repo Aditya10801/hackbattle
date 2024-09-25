6c04a52526ca0d4d7cab4fe80c800309b152266b
Utkarsh Malaiya <65324877+utkrshm@users.noreply.github.com>
2024-09-25 23:38:22 +0530

    update urls in localhost

--- hackbattle/calibr8/views.py
+++ hackbattle/calibr8/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'calibr8/index.html')
+
+def register(request):
+    return render(request, 'calibr8/register.html')
+
+def login(request):
+    return render(request, 'calibr8/login.html')
+
+def preferences(request):
+    return render(request, 'calibr8/settings.html')
+
+def scheduling(request):
+    return render(request, 'calibr8/timetable.html')
